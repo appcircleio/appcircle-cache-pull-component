e0cc00e588c42d4d54476ba4e26b2a2aada1883e
Onur Yılmaz <csonuryilmaz@gmail.com>
2022-02-22 16:35:09 +0300

    Update cache pull component
    - Adopt dependencies section to macOS agent
    - Change nonzero exit conditions with exit=0 and @@[error]
    - Add md5sum output and refactor @cache global var

--- main.rb
+++ main.rb
@@ -1,6 +1,7 @@
 require 'English'
 require 'net/http'
 require 'json'
+require 'digest'
 
 def get_env_variable(key)
   return nil if ENV[key].nil? || ENV[key].strip.empty?
@@ -9,31 +10,36 @@ def get_env_variable(key)
 end
 
 def run_command(command)
+  unless system(command)
+    puts "Unexpected exit with code #{$CHILD_STATUS.exitstatus}. Check logs for details."
+    exit 0
+  end
+end
+
+def run_command_with_log(command)
   puts "@@[command] #{command}"
-  exit $CHILD_STATUS.exitstatus unless system(command)
+  run_command(command)
 end
 
-def run_command_silent(command)
-  exit $CHILD_STATUS.exitstatus unless system(command)
-end
-
-def install_deps_if_not_exist(tool)
-  run_command_silent("dpkg -s #{tool} > /dev/null 2>&1 || apt-get -y install #{tool} > /dev/null 2>&1")
+def abort_with0(message)
+  puts "@@[error] #{message}"
+  exit 0
 end
 
 ac_repository_path = get_env_variable('AC_REPOSITORY_DIR')
-ac_cache_label = get_env_variable('AC_CACHE_LABEL') || abort('Cache label path must be defined.')
+ac_cache_label = get_env_variable('AC_CACHE_LABEL') || abort_with0('Cache label path must be defined.')
 
-ac_token_id = get_env_variable('AC_TOKEN_ID') || abort('AC_TOKEN_ID env variable must be set when build started.')
+ac_token_id = get_env_variable('AC_TOKEN_ID') || abort_with0('AC_TOKEN_ID env variable must be set when build started.')
 ac_callback_url = get_env_variable('ASPNETCORE_CALLBACK_URL') ||
-                  abort('ASPNETCORE_CALLBACK_URL env variable must be set when build started.')
+                  abort_with0('ASPNETCORE_CALLBACK_URL env variable must be set when build started.')
 
 signed_url_api = "#{ac_callback_url}?action=getCacheUrls"
 
-install_deps_if_not_exist('curl')
-install_deps_if_not_exist('unzip')
+# check dependencies
+run_command('unzip -v |head -1')
+run_command('curl --version |head -1')
 
-@cache = "ac_cache/#{ac_cache_label}"
+cache = "ac_cache/#{ac_cache_label}"
 zipped = "ac_cache/#{ac_cache_label.gsub('/', '_')}.zip"
 
 puts 'Inputs:'
@@ -41,8 +47,8 @@ puts ac_cache_label
 puts ac_repository_path
 puts '------'
 
-system("rm -rf #{@cache}")
-system("mkdir -p #{@cache}")
+system("rm -rf #{cache}")
+system("mkdir -p #{cache}")
 
 unless ac_token_id.empty?
   puts ''
@@ -58,23 +64,28 @@ unless ac_token_id.empty?
     puts signed['getUrl']
 
     ENV['AC_CACHE_GET_URL'] = signed['getUrl']
-    run_command_silent("curl -X GET -H \"Content-Type: application/zip\" -o #{zipped} $AC_CACHE_GET_URL")
+    run_command("curl -X GET -H \"Content-Type: application/zip\" -o #{zipped} $AC_CACHE_GET_URL")
   end
 end
 
 exit 0 unless File.size?(zipped)
 exit 0 if system("head -1 #{zipped} |grep -i -q NoSuchKey && rm -f #{zipped}")
 
-run_command("unzip -qq #{zipped}")
+md5sum = Digest::MD5.file(zipped).hexdigest
+puts "MD5: #{md5sum}"
+File.open("#{zipped}.md5", 'a') do |f|
+  f.puts md5sum.to_s
+end
+run_command_with_log("unzip -qq #{zipped}")
 
-Dir.glob("#{@cache}/*.zip", File::FNM_DOTMATCH).each do |zip_file|
-  run_command("unzip -qq #{zip_file} -d /")
+Dir.glob("#{cache}/*.zip", File::FNM_DOTMATCH).each do |zip_file|
+  run_command_with_log("unzip -qq #{zip_file} -d /")
 end
 
-Dir.glob("#{@cache}/repository/*.zip", File::FNM_DOTMATCH).each do |zip_file|
+Dir.glob("#{cache}/repository/*.zip", File::FNM_DOTMATCH).each do |zip_file|
   if ac_repository_path
-    run_command("unzip -qq -u #{zip_file} -d #{ac_repository_path}")
+    run_command_with_log("unzip -qq -u #{zip_file} -d #{ac_repository_path}")
   else
-    puts "Warning: #{zip_file} is ignored. It can be used only after Git Clone workflow step."
+    puts "Warning: #{zip_file} is skipped. It can be used only after Git Clone workflow step."
   end
 end
