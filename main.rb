17029e0c9d2b114d5531b7993fa001d1cc5945f3
Onur Yılmaz <csonuryilmaz@gmail.com>
2022-03-07 12:13:00 +0300

    Update cache pull > 1.0.0 (#1)
    - Change nonzero exit conditions with exit=0 and @@[error]
    - Add md5sum output and refactor @cache global var
    - Add @@[error] label to subcommand exit error message
    - Change callback url env variable name (web service)
    - Add dynamic system paths to cache pull (env vars)

--- main.rb
+++ main.rb
@@ -1,6 +1,7 @@
 require 'English'
 require 'net/http'
 require 'json'
+require 'digest'
 
 def get_env_variable(key)
   return nil if ENV[key].nil? || ENV[key].strip.empty?
@@ -9,40 +10,56 @@ def get_env_variable(key)
 end
 
 def run_command(command)
+  unless system(command)
+    puts "@@[error] Unexpected exit with code #{$CHILD_STATUS.exitstatus}. Check logs for details."
+    exit 0
+  end
+end
+
+def run_command_with_log(command)
   puts "@@[command] #{command}"
-  exit $CHILD_STATUS.exitstatus unless system(command)
+  s = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+  run_command(command)
+  e = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+  puts "took #{(e - s).round(3)}s"
 end
 
-def run_command_silent(command)
-  exit $CHILD_STATUS.exitstatus unless system(command)
-end
-
-def install_deps_if_not_exist(tool)
-  run_command_silent("dpkg -s #{tool} > /dev/null 2>&1 || apt-get -y install #{tool} > /dev/null 2>&1")
+def abort_with0(message)
+  puts "@@[error] #{message}"
+  exit 0
 end
 
 ac_repository_path = get_env_variable('AC_REPOSITORY_DIR')
-ac_cache_label = get_env_variable('AC_CACHE_LABEL') || abort('Cache label path must be defined.')
+ac_cache_label = get_env_variable('AC_CACHE_LABEL') || abort_with0('Cache label path must be defined.')
 
-ac_token_id = get_env_variable('AC_TOKEN_ID') || abort('AC_TOKEN_ID env variable must be set when build started.')
-ac_callback_url = get_env_variable('ASPNETCORE_CALLBACK_URL') ||
-                  abort('ASPNETCORE_CALLBACK_URL env variable must be set when build started.')
+ac_token_id = get_env_variable('AC_TOKEN_ID') || abort_with0('AC_TOKEN_ID env variable must be set when build started.')
+ac_callback_url = get_env_variable('AC_CALLBACK_URL') ||
+                  abort_with0('AC_CALLBACK_URL env variable must be set when build started.')
 
 signed_url_api = "#{ac_callback_url}?action=getCacheUrls"
 
-install_deps_if_not_exist('curl')
-install_deps_if_not_exist('unzip')
+# check dependencies
+run_command('unzip -v |head -1')
+run_command('curl --version |head -1')
 
-@cache = "ac_cache/#{ac_cache_label}"
+cache = "ac_cache/#{ac_cache_label}"
 zipped = "ac_cache/#{ac_cache_label.gsub('/', '_')}.zip"
 
-puts 'Inputs:'
+puts '--- Inputs:'
 puts ac_cache_label
 puts ac_repository_path
-puts '------'
+puts '-----------'
 
-system("rm -rf #{@cache}")
-system("mkdir -p #{@cache}")
+env_dirs = Hash.new('')
+ENV.each_pair do |k, v|
+  next unless k.start_with?('AC_')
+  next if v.include?('//') || v.include?(':')
+
+  env_dirs[k] = v if File.directory?(v) || %r{^(.+)/([^/]+)$} =~ v
+end
+
+system("rm -rf #{cache}")
+system("mkdir -p #{cache}")
 
 unless ac_token_id.empty?
   puts ''
@@ -54,27 +71,32 @@ unless ac_token_id.empty?
   response = Net::HTTP.get(uri)
   unless response.empty?
     puts 'Downloading cache...'
-    signed = JSON.parse(response)
-    puts signed['getUrl']
 
+    signed = JSON.parse(response)
     ENV['AC_CACHE_GET_URL'] = signed['getUrl']
-    run_command_silent("curl -X GET -H \"Content-Type: application/zip\" -o #{zipped} $AC_CACHE_GET_URL")
+    puts ENV['AC_CACHE_GET_URL']
+    run_command_with_log("curl -X GET -H \"Content-Type: application/zip\" -o #{zipped} $AC_CACHE_GET_URL")
   end
 end
 
 exit 0 unless File.size?(zipped)
 exit 0 if system("head -1 #{zipped} |grep -i -q NoSuchKey && rm -f #{zipped}")
 
-run_command("unzip -qq #{zipped}")
-
-Dir.glob("#{@cache}/*.zip", File::FNM_DOTMATCH).each do |zip_file|
-  run_command("unzip -qq #{zip_file} -d /")
+md5sum = Digest::MD5.file(zipped).hexdigest
+puts "MD5: #{md5sum}"
+File.open("#{zipped}.md5", 'a') do |f|
+  f.puts md5sum.to_s
 end
+run_command_with_log("unzip -qq #{zipped}")
 
-Dir.glob("#{@cache}/repository/*.zip", File::FNM_DOTMATCH).each do |zip_file|
-  if ac_repository_path
-    run_command("unzip -qq -u #{zip_file} -d #{ac_repository_path}")
-  else
-    puts "Warning: #{zip_file} is ignored. It can be used only after Git Clone workflow step."
-  end
+Dir.glob("#{cache}/**/*.zip", File::FNM_DOTMATCH).each do |zip_file|
+  puts zip_file
+
+  last_slash = zip_file.rindex('/')
+  base_path = zip_file[cache.length..last_slash - 1]
+  base_path = env_dirs[base_path[1..-1]] if env_dirs.key?(base_path[1..-1])
+
+  puts base_path
+  system("mkdir -p #{base_path}")
+  run_command_with_log("unzip -qq -u #{zip_file} -d #{base_path}/")
 end
